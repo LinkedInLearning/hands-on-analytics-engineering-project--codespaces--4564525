6cb536cfa42c5a91c0dd38befd80bd001002fe2f
Connor Dickson <68393151+cdcksn@users.noreply.github.com>
2024-04-26 06:16:46 +0000

    03_02

--- queries/customer_orders_query.sql
+++ queries/customer_orders_query.sql
@@ -5,3 +5,8 @@ from customer_orders_table
 select *
 from customer_orders_table
 where order_number is null 
+
+select *
+from customer_shipping_table
+inner join customer_orders_table
+  on customer_shipping_table.order_number = customer_orders_table.order_number
