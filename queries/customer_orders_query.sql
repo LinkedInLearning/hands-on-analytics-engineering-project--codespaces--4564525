5e2053a0380d8cd92da7bac2bab90697014e4cbf
Connor Dickson <68393151+cdcksn@users.noreply.github.com>
2024-04-26 06:03:03 +0000

    02_04

--- queries/customer_orders_query.sql
+++ queries/customer_orders_query.sql
@@ -1,2 +1,7 @@
 select * 
+  , row_number() over(partition by order_number, order_total, order_date order by order_date desc) as row_number
 from customer_orders_table
+
+select *
+from customer_orders_table
+where order_number is null 
